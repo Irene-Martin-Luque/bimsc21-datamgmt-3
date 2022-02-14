93673eaea61fb7aca3189828c77cc4b9d035b53a
Irene-Martin-Luque <91753032+Irene-Martin-Luque@users.noreply.github.com>
2022-02-14 23:45:34 +0100

    example2.2 color

--- example2.2 color/script.js
+++ example2.2 color/script.js
@@ -153,14 +153,14 @@ let scene, camera, renderer, controls;
 function init() {
   // create a scene and a camera
   scene = new THREE.Scene();
-  scene.background = new THREE.Color(1, 1, 1);
+  scene.background = new THREE.Color(0, 0, 0);
   camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
   );
-  camera.position.z = -30;
+  camera.position.z = -150;
 
   // create the renderer and add it to the html
   renderer = new THREE.WebGLRenderer({ antialias: true });
